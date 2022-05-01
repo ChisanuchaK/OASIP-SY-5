d5f6d7131feae1b989d4a24277d94bd34027aadc
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2022-05-02 03:34:35 +0700

    add get

--- TimeUpBackend/src/main/java/sit/int221/TimeUpBackend/DTO/BookingMoreDetailDTO.java
+++ TimeUpBackend/src/main/java/sit/int221/TimeUpBackend/DTO/BookingMoreDetailDTO.java
@@ -1,6 +1,7 @@
 package sit.int221.TimeUpBackend.DTO;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,8 +19,13 @@ public class BookingMoreDetailDTO {
     @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private Date eventStartTime;
     private Integer eventDuration;
+    @JsonIgnore
     private EventCategory eventCategory;
+    private String eventCategoryName;
     private String bookingName;
     private String bookingEmail;
     private String EventNote;
+    public String  getEventCategoryName(){
+        return eventCategory.getEventCategoryName();
+    }
 }
