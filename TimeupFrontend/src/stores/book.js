64f469aaa1f0de5302e97335dd1a8244aadf8a9e
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2022-05-09 16:23:03 +0700

    add alert method create

--- TimeupFrontend/src/stores/book.js
+++ TimeupFrontend/src/stores/book.js
@@ -74,7 +74,7 @@ export const createBooking = async (localData) => {
   if (res.status === 201) {
     // const addBooking = await res.json()
     // bookingsList.value.push(addBooking)
-    alert(`Create successfully  ${localData.eventCategory.eventCategoryName} \n ${localData.evenyStartTime} \n ${localData.bookingName}`)
+    alert(`Create successfully \n Category :  ${localData.eventCategory.eventCategoryName} \n Date : ${localData.eventStartTime} \n Booking name :  ${localData.bookingName}`)
   } else {
     console.log("error , failed to created");
   }
