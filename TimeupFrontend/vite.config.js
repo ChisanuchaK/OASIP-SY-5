60d0b25b9c80229b25d5b05ae3b568f564711e57
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2022-05-10 19:54:46 +0700

    add nginx container

--- TimeupFrontend/vite.config.js
+++ TimeupFrontend/vite.config.js
@@ -5,15 +5,15 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "/sy5/",
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://intproj21.sit.kmutt.ac.th",
-  //       // target: "http://ip21sy5.sit.kmutt.ac.th/",
-  //       changeOrigin: true,
-  //       secure: false,
-  //       // rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://intproj21.sit.kmutt.ac.th",
+        // target: "http://ip21sy5.sit.kmutt.ac.th/",
+        changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
