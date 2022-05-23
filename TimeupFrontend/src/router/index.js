c1c51cdfed05e029b19532b237b7f7dc01e1a8b6
thanapat63130500049 <thanapat.tle@mail.kmutt.ac.th>
2022-05-23 21:45:50 +0700

    update18-230565-2

--- TimeupFrontend/src/router/index.js
+++ TimeupFrontend/src/router/index.js
@@ -5,7 +5,7 @@ import About from "../views/About.vue";
 import Reserve from "../views/Reserve.vue";
 import CategoryList from "../views/CategoryList.vue";
 
-const history = createWebHistory("/sy5");
+const history = createWebHistory(`${import.meta.env.VITE_ROUTER_URL}`);
 const routes = [
     {
         path: "/",
