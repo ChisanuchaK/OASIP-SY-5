61a137e96e2e693bc2c891c834c5f479251e72b4
thanapatLe <thanapat.skt@gmail.com>
2022-09-09 18:28:04 +0700

    push-Autentication[02]-s4-090965

--- TimeupFrontend/vite.config.js
+++ TimeupFrontend/vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-  // base: '/sy5/'
+  plugins: [vue()],
+  base: '/sy5/'
   // server: {
   //   proxy: {
   //     '/api': {
