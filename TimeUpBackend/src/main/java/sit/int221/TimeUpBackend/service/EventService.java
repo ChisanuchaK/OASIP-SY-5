c66a614fa7278028bf574eab3fe311f48a13d935
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2022-11-28 19:30:49 +0700

    test function delete file

--- TimeUpBackend/src/main/java/sit/int221/TimeUpBackend/service/EventService.java
+++ TimeUpBackend/src/main/java/sit/int221/TimeUpBackend/service/EventService.java
@@ -310,11 +310,11 @@ public class EventService{
             eventRepository.saveAndFlush(event);
         }
         else {
-            if(multipartFile == null &&  editEventPutDTO.getFileName() != null){
+            if(editEventPutDTO.getFileName() != null){
                 System.out.println(2);
                 eventRepository.saveAndFlush(event);
             }
-            else {
+            else if (multipartFile != null && editEventPutDTO.getFileName() == null) {
                 System.out.println(3);
                 storageService.deleteById(id);
                 storageService.save(multipartFile , id);
