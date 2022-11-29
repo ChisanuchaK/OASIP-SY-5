6dab76b0e099576b36d4ab6bcbccea7a06a979f6
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2022-11-29 11:03:22 +0700

    test function delete file

--- TimeUpBackend/src/main/java/sit/int221/TimeUpBackend/service/EventService.java
+++ TimeUpBackend/src/main/java/sit/int221/TimeUpBackend/service/EventService.java
@@ -285,14 +285,14 @@ public class EventService{
                  if(user.getRoleUser().equals("admin")){
                      System.out.println(event.getFileName());
                      System.out.println(event.getBookingEmail());
-                     return conditionEditEvent(editEventPutDTO , multipartFile, id, sizeByte, event);
+                     return conditionEditEvent(multipartFile, id, sizeByte, event);
                  }
                  else if (user.getRoleUser().equals("student") && event.getBookingEmail().equals(getCurrentAuthentication.getUsername())){
                      if(!(event.getBookingEmail().equals(user.getEmailUser()))){
                          throw  new ResponseStatusException(HttpStatus.FORBIDDEN , "email is not the same as student's email");
                      }
                      else{
-                         return conditionEditEvent(editEventPutDTO , multipartFile, id, sizeByte, event);
+                         return conditionEditEvent(multipartFile, id, sizeByte, event);
                      }
                  }
         }
@@ -302,23 +302,22 @@ public class EventService{
         throw  new ResponseStatusException(HttpStatus.FORBIDDEN , "This email permission denied");
     }
 
-    private ResponseEntity conditionEditEvent(EventPutDto editEventPutDTO , MultipartFile multipartFile, Integer id, int sizeByte, Event event) throws IOException {
-        if(multipartFile != null && event.getFileName() == null){
+    private ResponseEntity conditionEditEvent(MultipartFile multipartFile, Integer id, int sizeByte, Event event) throws IOException {
+        if(event.getFileName() == null){
             System.out.println(1);
             storageService.save(multipartFile , id);
             event.setFileSize(sizeByte);
             eventRepository.saveAndFlush(event);
         }
         else {
-            if(multipartFile == null && editEventPutDTO.getFileName() != null ){
+            if(multipartFile == null){
                 System.out.println(2);
+                if(event.getFileName() != null){
+                    storageService.deleteById(id);
+                }
                 eventRepository.saveAndFlush(event);
-            } else if (multipartFile == null) {
-                storageService.deleteById(id);
-                event.setFileSize(0);
-                eventRepository.saveAndFlush(event);
-
-            } else {
+            }
+            else {
                 System.out.println(3);
                 storageService.deleteById(id);
                 storageService.save(multipartFile , id);
